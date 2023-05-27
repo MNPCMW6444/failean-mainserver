317986c9072899178ef70f71beef925f64ed70b4
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-05-27 20:54:02 +0300

    Update email.ts

--- src/app/external-api-s/email.ts
+++ src/app/external-api-s/email.ts
@@ -7,7 +7,8 @@ sgMail.setApiKey(process.env.SENDGRID || "");
 export const sendEmail = (to: string, subject: string, html: string) =>
   sgMail.send({
     from: {
-      email: "service@f-ai-ler.com",
+      //email: "service@f-ai-ler.com",
+      email: "temp@caphub.ai",
       name: "fAIler",
     },
     to,
