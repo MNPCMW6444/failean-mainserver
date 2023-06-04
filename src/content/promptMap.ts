2fe754091d03a9b85802b8f603fd752e53106de4
YoadNatan <106985580+YoadNatan@users.noreply.github.com>
2023-06-05 00:51:45 +0300

    Update promptMap.ts

--- src/content/promptMap.ts
+++ src/content/promptMap.ts
@@ -82,37 +82,6 @@ const promptMap: PromptMap = {
     },
 
   ],
-  problemStatment: [
-    {
-      type: "static",
-      content:
-        "You are now a Co-Founder and an expireinced entreprenuer. Youre goal is to write a good problem statmrnt. A good problem statement will cover what is the problem, why it's important, and who it impacts (Try giving the number of people/the amount of money lost). A good problem statement should should create awareness and stimulate creative thinking. Define the problem statment shorly based on the following Idea .\n",
-    },
-    { type: "variable", content: "idea" },
-  ],
-  visionStatment: [
-    {
-      type: "static",
-      content:
-        "Define the vision statment in one sentence. It should be clear and apealing and deliver the positive change the idea will have in the world. use this idea and problem statment to detemine the vision statment   \n",
-    },
-    { type: "variable", content: "idea" },
-    { type: "variable", content: "problemStatment" },
-  ],
-  solution: [
-    {
-      type: "static",
-      content:
-        "Define the soulution statment based on the following Idea, vision and problem statments. \n",
-    },
-    { type: "variable", content: "idea\n " },
-    { type: "variable", content: "visionStatment\n " },
-    { type: "variable", content: "problemStatment\n " },
-    {
-      type: "static",
-      content:
-        "A good solution statement will cover what is the best practical and effective way to use the idea to solve the problem. It needs to show exactly how the idea solve each part of the problem and the overall problem",
-    },
-  ],
+
 };
 export default promptMap;
