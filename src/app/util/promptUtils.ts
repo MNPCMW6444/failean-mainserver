c6416ba7c63e94bc869d7ce34e13d1fa67882c94
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-05 02:20:06 +0300

    Update promptUtils.ts

--- src/app/util/promptUtils.ts
+++ src/app/util/promptUtils.ts
@@ -24,5 +24,29 @@ export const dependencyMapper = (promptMap: PromptMap) => {
     }
   }
 
+  const countDependents = (dependencyTree: any, promptName: string): number => {
+    let count = 0;
+    const dependents = dependencyTree[promptName];
+    if (!dependents) {
+      return count;
+    }
+
+    if (Array.isArray(dependents)) {
+      dependents.forEach((dependent) => {
+        count += 1 + countDependents(dependencyTree, dependent);
+      });
+    } else {
+      for (const dependent in dependents) {
+        count += 1 + countDependents(dependencyTree, dependent);
+      }
+    }
+
+    return count;
+  };
+
+  // Usage example:
+  const numberOfDependents = countDependents(dependencyTree, "problemStatment");
+  console.log(numberOfDependents);
+
   return dependencyTree;
 };
