6ae74815b5590505e55c4d41a287cba1bc663712
YoadNatan <106985580+YoadNatan@users.noreply.github.com>
2023-06-06 01:03:14 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,25 @@ try {
   if (process.env.YOAD_FLAG === "dflkgmgj") {
     const tree = dependencyMapper(promptMap);
     const order = getDependencyOrder(tree, "idea");
-    console.log(order);
-    console.log("total: ", order.length);
+    //console.log(order);
+    //console.log("total: ", order.length);
+
+
   }
 } catch (e) {}
+
+
+const sf=(name:string)=>{
+let deps:any=[];
+promptMap[name].forEach((prat)=>{if (prat.type==="variable") deps.push(prat.content)}) 
+  deps= deps.map((dep:string)=>{
+    if(dep==="idea") return dep
+return {
+  [dep]:sf(dep)
+}
+
+  });
+  return deps
+}
+
+console.log( sf("pricing"));
