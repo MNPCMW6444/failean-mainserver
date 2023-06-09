ef5df22d9a84c697c7a4c448f0ba5973401afcf2
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-06-09 20:23:14 +0300

    g

--- src/app/util/promptUtils.ts
+++ src/app/util/promptUtils.ts
@@ -1,30 +1,43 @@
-export const convertMaptoGraph = (promptMap) => {
-  let superPrompts = Object.keys(promptMap).map(promptName => ({
-    name: promptName,
-    deps: promptMap[promptName].filter(promptPart => promptPart.type === "variable" && promptPart.content),
-    level: 0,
-  }));
-  superPrompts.unshift({ name: "idea", deps: [], level: 0 });
+import { PromptMap, PromptPart } from "@failean/shared-types";
 
-  let level = 0;
-  while (superPrompts.some(sp => sp.level < 1)) {
-    level++;
-    superPrompts.filter(sp => sp.level === -1).forEach(sp => {
-      sp.level = level - 1;
-      superPrompts = [...superPrompts.filter(s => s.name !== sp.name), sp];
-    });
+export const convertMaptoGraph = (promptMap: PromptMap) => {
+  let superPrompts: { name: string; deps: string[]; level: number }[] = Object.keys(promptMap).map(promptName => ({
+      name: promptName,
+          deps: promptMap[promptName]
+                .filter((promptPart: PromptPart) => promptPart.type === "variable" && promptPart.content)
+                      .map((promptPart: PromptPart) => promptPart.content || '') as string[],
+                          level: 0,
+                            }));
 
-    superPrompts.filter(sp => !sp.level).forEach(sp => {
-      const satisfied = sp.deps.every(name => {
-        const number = superPrompts.find(spx => spx.name === name)?.level;
-        return number && number > 0;
-      });
-      if (satisfied) {
-        sp.level = -1;
-        superPrompts = [...superPrompts.filter(s => s.name !== sp.name), sp];
-      }
-    });
-  }
+                              superPrompts.unshift({ name: "idea", deps: [], level: 0 });
 
-  return superPrompts.map(({ name, level }) => ({ name, level }));
-};
+                                let level = 0;
+
+                                  while (superPrompts.some(({ level }) => level < 1)) {
+                                      level++;
+
+                                          superPrompts
+                                                .filter(({ level }) => level === -1)
+                                                      .forEach(sp => {
+                                                              sp.level = level - 1;
+                                                                      superPrompts = [...superPrompts.filter(({ name }) => name !== sp.name), sp];
+                                                                            });
+
+                                                                                superPrompts
+                                                                                      .filter(({ level }) => level === 0)
+                                                                                            .forEach(sp => {
+                                                                                                    const satisfied = sp.deps.every(name => {
+                                                                                                              const number = superPrompts.find(spx => spx.name === name)?.level;
+                                                                                                                        return number && number > 0;
+                                                                                                                                });
+
+                                                                                                                                        if (satisfied) {
+                                                                                                                                                  sp.level = -1;
+                                                                                                                                                            superPrompts = [...superPrompts.filter(({ name }) => name !== sp.name), sp];
+                                                                                                                                                                    }
+                                                                                                                                                                          });
+                                                                                                                                                                            }
+
+                                                                                                                                                                              return superPrompts.map(({ name, level }) => ({ name, level }));
+                                                                                                                                                                              };
+                                                                                                                                                                              
