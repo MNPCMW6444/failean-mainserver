7001c040178ad9d2c2929b339731d11a9dab8507
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-13 22:46:07 +0300

    lilush

--- src/content/email-templates/authEmails.ts
+++ src/content/email-templates/authEmails.ts
@@ -1,4 +1,4 @@
-const signupreq = (url: string) => ({
+export const signupreq = (url: string) => ({
   subject: "Activate your Failean Account",
   body: `<!DOCTYPE html>
   <html lang="en">
@@ -22,7 +22,7 @@ const signupreq = (url: string) => ({
   `,
 });
 
-const passreset = (url: string) => ({
+export const passreset = (url: string) => ({
   subject: "Failean Password Reset Request",
   body: `<!DOCTYPE html>
     <html lang="en">
@@ -45,4 +45,26 @@ const passreset = (url: string) => ({
     `,
 });
 
-export { signupreq, passreset };
+export const websiteSignup = (url: string) => ({
+  subject: "Activate your Failean Account",
+  body: `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Activate Your Failean Account</title>
+  </head>
+  <body>
+      <div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 5px; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); background-color: #003399; color: #ffffff;">
+          <h2>Welcome to Failean!</h2>
+          <p>At Failean, we celebrate the learning journey inherent in failures. Our platform empowers entrepreneurs to fail fast, learn faster, and move closer to success. We're delighted that you've joined our community of innovative thinkers and doers.</p>
+          <p>Activate your account by clicking on the button below:</p>
+          <a href="${url}" style="background-color: #ff6600; color: #ffffff; text-decoration: none; padding: 15px 30px; border-radius: 5px; display: inline-block; margin-bottom: 20px;">Activate My Account</a>
+          <p>If you did not sign up for our platform, please disregard this email.</p>
+          <p>Best regards,</p>
+          <p><strong>The Failean Team</strong></p>
+      </div>
+  </body>
+  </html>
+  `,
+});
