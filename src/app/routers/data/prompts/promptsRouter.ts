6d4718a5d7f1c643d89dabba3d1d398eca702d9e
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-06-19 17:34:38 +0300

    asd

--- src/app/routers/data/prompts/promptsRouter.ts
+++ src/app/routers/data/prompts/promptsRouter.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import PromptResultModel from "../../../mongo-models/data/prompts/promptResultModel";
 import jsonwebtoken, { JwtPayload } from "jsonwebtoken";
-import { PromptName } from "@failean/shared-types";
+import { PromptName, PromptPart } from "@failean/shared-types";
 import openAIQueue from "../../../util/openAIQueue";
 import { convertMaptoDepGraph } from "../../../util/data/prompts/promptUtil";
 import promptMap from "../../../../content/prompts/promptMap";
+import ideaModel from "src/app/mongo-models/data/ideas/ideaModel";
+import userModel from "src/app/mongo-models/auth/userModel";
 
 const router = express.Router();
 
@@ -49,110 +51,22 @@ router.post("/getPromptResult", async (req, res) => {
 });
 
 router.post("/runAndGetPromptResult", async (req, res) => {
-  const { user, roleName, chat } = req.body;
+  const token = req.cookies.jsonwebtoken;
+  if (!token) return res.status(401).json({ clientMessage: "Unauthorized" });
+  const validatedUser = jsonwebtoken.verify(
+    token,
+    process.env.JWT_SECRET as string
+  );
+  const user = await userModel.findById((validatedUser as JwtPayload).id);
 
-  const job = await openAIQueue.add({ user, roleName, chat });
+  const {
+    ideaId,
+    promptName,
+    feedback,
+  }: { ideaId: string; promptName: PromptName; feedback?: string } = req.body;
 
+  const job = await openAIQueue.add({ user, ideaId, promptName, feedback });
   res.status(200).json({ jobId: job.id });
-
-  // try {
-  //   const token = req.cookies.jsonwebtoken;
-  //   if (!token) return res.status(401).json({ clientMessage: "Unauthorized" });
-  //   const validatedUser = jsonwebtoken.verify(
-  //     token,
-  //     process.env.JWT_SECRET as string
-  //   );
-  //   const userId = (validatedUser as JwtPayload).id;
-
-  //   const {
-  //     ideaId,
-  //     promptName,
-  //     feedback,
-  //   }: { ideaId: string; promptName: PromptName; feedback?: string } = req.body;
-
-  //   const idea = await ideaModel.findById(ideaId);
-
-  //   let dependencies: string[];
-
-  //   const prompt = promptMap[promptName];
-  //   if (prompt) {
-  //     let promises = prompt.prompt.map(async (promptPart: PromptPart) => {
-  //       if (promptPart.type === "variable" && promptPart.content !== "idea") {
-  //         let promptRes = await PromptResultModel.find({
-  //           owner: userId,
-  //           ideaId,
-  //           promptName: promptPart.content,
-  //         });
-  //         return {
-  //           x: promptRes[promptRes.length - 1].data,
-  //         };
-  //       }
-  //     });
-  //     Promise.all(promises).then(async (updatedPropmtResult) => {
-  //       dependencies = updatedPropmtResult.map((r: any) => {
-  //         return r;
-  //       });
-
-  //       const cleanDeps: string[] = [];
-  //       dependencies.forEach((dep) => {
-  //         if (dep) cleanDeps.push(dep);
-  //       });
-  //       let i = 0;
-
-  //       const constructedPrompt = prompt.prompt.map(
-  //         (promptPart: PromptPart) => {
-  //           if (promptPart.type === "static") return promptPart.content;
-  //           else if (promptPart.type === "variable") {
-  //             if (promptPart.content === "idea") return idea?.idea;
-  //             i++;
-  //             return (cleanDeps[i - 1] as any)?.x;
-  //           }
-  //         }
-  //       );
-
-  //       const user = userModel.findById(userId);
-
-  //       const promptResult =
-  //         feedback?.length &&
-  //         feedback?.length > 1 &&
-  //         (await PromptResultModel.find({
-  //           owner: userId,
-  //           ideaId,
-  //           promptName,
-  //         }));
-
-  //       const completion = callOpenAI(
-  //         user as unknown as WhiteUser,
-  //         prompt.role,
-  //         promptResult && [promptResult.length - 1] &&
-  //           promptResult[promptResult.length - 1].data
-  //           ? [
-  //               { role: "user", content: constructedPrompt.join("") },
-  //               {
-  //                 role: "assistant",
-  //                 content: promptResult[promptResult.length - 1].data,
-  //               },
-  //               { role: "user", content: feedback },
-  //             ]
-  //           : [{ role: "user", content: constructedPrompt.join("") }]
-  //       );
-
-  //       const savedResult = new PromptResultModel({
-  //         owner: userId,
-  //         ideaId,
-  //         promptName,
-  //         data: (completion as any).data.choices[0].message?.content,
-  //       });
-  //       await savedResult.save();
-  //       return res.status(200).json({
-  //         response: savedResult.data,
-  //       });
-  //     });
-  //   }
-  // } catch (err) {
-  //   console.error(err);
-  //   return res.status(500).json({ errorMessage: JSON.stringify(err) });
-  // }
 });
 
 router.post("/savePromptResult", async (req, res) => {
--- src/app/util/openAIQueue.ts
+++ src/app/util/openAIQueue.ts
@@ -1,6 +1,8 @@
 import Queue from "bull";
 import { pubsub } from "../../index"; // assuming this is the correct relative path
 import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
+import ideaModel from "../mongo-models/data/ideas/ideaModel";
+import promptMap from "src/content/prompts/promptMap";
 
 // Setup OpenAI
 const configuration = new Configuration({
@@ -20,17 +22,83 @@ const processJob = async (job: any) => {
     return;
   }
 
-  // Make the actual API call here
-  const result = await openai.createChatCompletion({
-    model: "gpt-3", // Replace with your desired model
-    messages: [
-      {
-        role: "system",
-        content: role,
-      },
-      ...chat,
-    ],
-  });
+  const idea = await ideaModel.findById(ideaId);
+
+  let dependencies: string[];
+
+  const prompt = promptMap[promptName];
+  if (prompt) {
+    let promises = prompt.prompt.map(async (promptPart: PromptPart) => {
+      if (promptPart.type === "variable" && promptPart.content !== "idea") {
+        let promptRes = await PromptResultModel.find({
+          owner: userId,
+          ideaId,
+          promptName: promptPart.content,
+        });
+        return {
+          x: promptRes[promptRes.length - 1].data,
+        };
+      }
+    });
+    Promise.all(promises).then(async (updatedPropmtResult) => {
+      dependencies = updatedPropmtResult.map((r: any) => {
+        return r;
+      });
+
+      const cleanDeps: string[] = [];
+      dependencies.forEach((dep) => {
+        if (dep) cleanDeps.push(dep);
+      });
+      let i = 0;
+
+      const constructedPrompt = prompt.prompt.map((promptPart: PromptPart) => {
+        if (promptPart.type === "static") return promptPart.content;
+        else if (promptPart.type === "variable") {
+          if (promptPart.content === "idea") return idea?.idea;
+          i++;
+          return (cleanDeps[i - 1] as any)?.x;
+        }
+      });
+
+      const user = userModel.findById(userId);
+
+      const promptResult =
+        feedback?.length &&
+        feedback?.length > 1 &&
+        (await PromptResultModel.find({
+          owner: userId,
+          ideaId,
+          promptName,
+        }));
+
+      const completion = callOpenAI(
+        user as unknown as WhiteUser,
+        prompt.role,
+        promptResult && [promptResult.length - 1] &&
+          promptResult[promptResult.length - 1].data
+          ? [
+              { role: "user", content: constructedPrompt.join("") },
+              {
+                role: "assistant",
+                content: promptResult[promptResult.length - 1].data,
+              },
+              { role: "user", content: feedback },
+            ]
+          : [{ role: "user", content: constructedPrompt.join("") }]
+      );
+
+      const savedResult = new PromptResultModel({
+        owner: userId,
+        ideaId,
+        promptName,
+        data: (completion as any).data.choices[0].message?.content,
+      });
+      await savedResult.save();
+      return res.status(200).json({
+        response: savedResult.data,
+      });
+    });
+  
 
   // You could set job status in Redis here
 
