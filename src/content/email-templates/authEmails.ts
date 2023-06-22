4d216b78a33e254b8007f0fda18f91666353ebae
Rhythm1cal <itai.lilling@gmail.com>
2023-06-22 20:10:51 +0300

    Update authEmails.ts

--- src/content/email-templates/authEmails.ts
+++ src/content/email-templates/authEmails.ts
@@ -1,6 +1,7 @@
 const signupreq = (url: string) => ({
   subject: "Activate your Failean Account",
   body: `<!DOCTYPE html>
+  <!DOCTYPE html>
   <html lang="en">
   <head>
       <meta charset="UTF-8">
@@ -10,17 +11,31 @@ const signupreq = (url: string) => ({
   <body>
       <div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 5px; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); background-color: #003399; color: #ffffff;">
           <h2>Welcome to Failean!</h2>
-          <p>At Failean, we celebrate the learning journey inherent in failures. Our platform empowers entrepreneurs to fail fast, learn faster, and move closer to success. We're delighted that you've joined our community of innovative thinkers and doers.</p>
-          <p>Activate your account by clicking on the button below:</p>
-          <a href="${url}" style="background-color: #ff6600; color: #ffffff; text-decoration: none; padding: 15px 30px; border-radius: 5px; display: inline-block; margin-bottom: 20px;">Activate My Account</a>
+          <p>Hey [Recipient's Name],</p>
+  
+          <p>Congratulations on taking the first step towards epic fails and even more epic comebacks with Failean! We're thrilled to have you on board.</p>
+  
+          <p>Failean is designed to take your ideas, churn them through the wringer, and spit out a business plan that's been through the school of hard knocks. Market research, competitor analysis, problem and solution identification – we've got it all covered.</p>
+  
+          <div style="background-color: #0066cc; padding: 20px; border-radius: 5px">
+              <p style="text-align: center;">So to activate your account by clicking on the button below:</p>
+              <div style="text-align: center;">
+                  <a href="${url}" style="background-color: #ff6600; color: #ffffff; text-decoration: none; padding: 15px 30px; border-radius: 5px; display: inline-block; margin-bottom: 20px;">Activate My Account</a>
+              </div>
+          </div>
+  
+      </div>
+      <div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px;">        
           <p>If you did not sign up for our platform, please disregard this email.</p>
           <p>Best regards,</p>
-          <p><strong>The Failean Team</strong></p>
+          <p><strong>Team Failean</strong></p>
       </div>
   </body>
   </html>
+  
   `,
 });
+
 
 const passreset = (url: string) => ({
   subject: "Failean Password Reset Request",
@@ -36,10 +51,13 @@ const passreset = (url: string) => ({
             <h2>Reset Your Password</h2>
             <p>We received a request to reset your password. Click the button below to set a new password:</p>
             <a href="${url}" style="background-color: #32CD32; color: #ffffff; text-decoration: none; padding: 15px 30px; border-radius: 5px; display: inline-block; margin-bottom: 20px;">Reset My Password</a>
-            <p>If you did not request a password reset, please ignore this email.</p>
-            <p>Best regards,</p>
-            <p><strong>The Failean Team</strong></p>
+
         </div>
+<div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px;">
+                    <p>If you did not request a password reset, please ignore this email.</p>
+            <p>Best regards,</p>
+            <p><strong>Team Failean</strong></p>
+            </div>
     </body>
     </html>
     `,
