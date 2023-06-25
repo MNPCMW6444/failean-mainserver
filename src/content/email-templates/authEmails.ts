98fad062f801f45a5986b0adece2994fb263c5b9
Rhythm1cal <itai.lilling@gmail.com>
2023-06-25 15:42:08 +0300

    Update authEmails.ts

--- src/content/email-templates/authEmails.ts
+++ src/content/email-templates/authEmails.ts
@@ -37,26 +37,27 @@ const signupreq = (url: string) => ({
 const passreset = (url: string) => ({
   subject: "Failean Password Reset Request",
   body: `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Reset Your Failean Password</title>
-    </head>
-    <body>
-        <div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 5px; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); background-color: #003399; color: #ffffff;">
-            <h2>Reset Your Password</h2>
-            <p>We received a request to reset your password. Click the button below to set a new password:</p>
-            <a href="${url}" style="background-color: #32CD32; color: #ffffff; text-decoration: none; padding: 15px 30px; border-radius: 5px; display: inline-block; margin-bottom: 20px;">Reset My Password</a>
-
-        </div>
-<div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px;">
-                    <p>If you did not request a password reset, please ignore this email.</p>
-            <p>Best regards,</p>
-            <p><strong>Team Failean</strong></p>
-            </div>
-    </body>
-    </html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reset Your Failean Password</title>
+  </head>
+  <body>
+    <div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 5px; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); background-color: #003399; color: #ffffff;">
+      <h2>Reset Your Password</h2>
+      <p>We received a request to reset your password. Click the button below to set a new password:</p>
+      <a href="${url}" style="background-color: #32CD32; color: #ffffff; text-decoration: none; padding: 15px 30px; border-radius: 5px; display: inline-block; margin-bottom: 20px;">Reset My Password</a>
+    </div>
+    <div style="font-family: Helvetica, Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px;">
+      <p>If you did not request a password reset, please ignore this email.</p>
+      <p>Best regards,</p>
+      <p>
+        <strong>Team Failean</strong>
+      </p>
+    </div>
+  </body>
+</html>y
     `,
 });
 
