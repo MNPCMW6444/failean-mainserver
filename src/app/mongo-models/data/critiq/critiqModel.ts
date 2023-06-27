cc09678ade38e34a14360f6924557f5e89a78d82
YoadNatan <yoad12121223@gmail.com>
2023-06-27 19:14:11 +0300

    criti

---
+++ src/app/mongo-models/data/critiq/critiqModel.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { WhiteModels } from "@failean/shared-types";
+
+type WhiteCritiq = WhiteModels.Data.Critiq.WhiteCritiq;
+
+interface Answer {
+  question: string;
+  selectedOption: string;
+  additionalDetail?: string;
+}
+
+interface Step {
+  title: string;
+  answers: Answer[];
+}
+
+const answerSchema = new mongoose.Schema<Answer>(
+  {
+    question: { type: String, required: true },
+    selectedOption: { type: String, required: true },
+    additionalDetail: String,
+  },
+  { _id: false }
+);
+
+const stepSchema = new mongoose.Schema<Step>(
+  {
+    title: { type: String, required: true },
+    answers: [answerSchema],
+  },
+  { _id: false }
+);
+
+const critiqSchema = new mongoose.Schema<WhiteCritiq>(
+  {
+    owner: { type: Schema.Types.ObjectId, required: true },
+    steps: [stepSchema],
+    archived: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<WhiteCritiq>("Critiq", critiqSchema);
