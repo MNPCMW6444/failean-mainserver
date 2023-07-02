b2c182aa739b15aca13c77b6a0b975d0810396eb
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-02 14:48:35 +0300

    migration

--- src/app/mongo-models/auth/requestForPassChangeModal.ts
+++ src/app/mongo-models/auth/requestForPassChangeModal.ts
@@ -1,24 +1,8 @@
 import mongoose from "mongoose";
 import { WhiteModels } from "@failean/shared-types";
-type WhiteRequestForPassChange = WhiteModels.Auth.WhiteRequestForPassChange;
+import { requestForPassChangeModel } from "@failean/mongo-models";
 
-const requestForPassChangeModal = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-    },
-    key: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-export default mongoose.model<WhiteRequestForPassChange>(
+export default mongoose.model<WhiteModels.Auth.WhiteRequestForPassChange>(
   "requestForPassChange",
-  requestForPassChangeModal
+  requestForPassChangeModel
 );
--- src/app/mongo-models/data/ideas/ideaModel.ts
+++ src/app/mongo-models/data/ideas/ideaModel.ts
@@ -1,22 +1,8 @@
 import mongoose from "mongoose";
 import { WhiteModels } from "@failean/shared-types";
-type WhiteIdea = WhiteModels.Data.Ideas.WhiteIdea;
+import { ideaModel } from "@failean/mongo-models";
 
-const ideaModal = new mongoose.Schema(
-  {
-    owner: { type: mongoose.Schema.Types.ObjectId, required: true },
-    idea: {
-      type: String,
-      required: true,
-    },
-    archived: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
+export default mongoose.model<WhiteModels.Data.Ideas.WhiteIdea>(
+  "idea",
+  ideaModel
 );
-
-export default mongoose.model<WhiteIdea>("idea", ideaModal);
