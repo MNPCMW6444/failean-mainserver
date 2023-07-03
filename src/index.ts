13eb0443004e29e8a81cddd915d4030e25c794f6
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-03 16:44:21 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,8 @@ const connectToDBs = () => {
 
 connectToDBs();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 export const ocURL =
   process.env.NODE_ENV === "development"
