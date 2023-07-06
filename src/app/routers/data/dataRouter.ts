5bc3a90e7ed853073e0616486d7f340d00b5b5dc
YoadNatan <yoad12121223@gmail.com>
2023-07-06 14:41:16 +0200

    Update dataRouter.ts

--- src/app/routers/data/dataRouter.ts
+++ src/app/routers/data/dataRouter.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import ideasRouter from "./ideas/ideasRouter";
 import promptsRouter from "./prompts/promptsRouter";
-
+import critIQustionireRouter from "./critiqQuestionire/critIQustionireRouter";
 const router = express.Router();
 
 router.use("/ideas", ideasRouter);
 
 router.use("/prompts", promptsRouter);
+
+router.use("/critiqQuestionire", critIQustionireRouter);
 
 export default router;
