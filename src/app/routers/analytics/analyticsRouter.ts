6247cbe82f1c7d458d2590d70bf891251a06c799
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-07-07 14:31:26 +0300

    asd

--- src/app/routers/analytics/analyticsRouter.ts
+++ src/app/routers/analytics/analyticsRouter.ts
@@ -5,12 +5,30 @@ import axios from "axios";
 const router = express.Router();
 
 router.post("/render", async (req, res) => {
-  axios.post(ocServerDomain + "/log/logPage", { ...req.body });
+  axios.post(
+    ocServerDomain + "/log/logPage",
+    { ...req.body },
+    {
+      auth: {
+        username: "client",
+        password: process.env.OCPASS + "",
+      },
+    }
+  );
   return res.status(200);
 });
 
 router.post("/sidebar", async (req, res) => {
-  axios.post(ocServerDomain + "/log/logSidebar", { ...req.body });
+  axios.post(
+    ocServerDomain + "/log/logSidebar",
+    { ...req.body },
+    {
+      auth: {
+        username: "client",
+        password: process.env.OCPASS + "",
+      },
+    }
+  );
   return res.status(200);
 });
 export default router;
