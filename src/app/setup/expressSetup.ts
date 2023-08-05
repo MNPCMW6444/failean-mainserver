997f84fb1340011e49a82251275e84359db98001
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-05 13:52:57 +0300

    sef

--- src/app/setup/expressSetup.ts
+++ src/app/setup/expressSetup.ts
@@ -7,21 +7,8 @@ import { clientDomain, ocClientDomain } from "./config";
 import routers from "../routers";
 import { discoverService } from "./AWSDiscovery";
 
-const {
-  authRouter,
-  accountsRouter,
-  websiteRouter,
-  dataRouter,
-  gqlRouter,
-  analyticsRouter,
-} = routers;
-
 export const app = express();
 export const port = process.env.PORT || 6555;
-
-const axiosLogger = (req: Request, res: Response, next: NextFunction) => {
-  next();
-};
 
 export let ocServerDomain = "";
 
@@ -30,46 +17,56 @@ discoverService("us-east-1", {
   ServiceName: "ocserver",
   MaxResults: 10,
 }).then((ip) => {
-  ocServerDomain = `http://${ip}:6777`;
-  const applyMiddlewares = (app: express.Application) => {
-    const middlewares = [
-      cookieParser(),
-      express.json({ limit: "50mb" }),
-      express.urlencoded({ limit: "50mb", extended: true }),
-      cors({
-        origin: [ocClientDomain, ocServerDomain, clientDomain],
-        credentials: true,
-      }),
-      axiosLogger,
-    ];
-
-    middlewares.forEach((middleware) => app.use(middleware));
+  const {
+    authRouter,
+    accountsRouter,
+    websiteRouter,
+    dataRouter,
+    gqlRouter,
+    analyticsRouter,
+  } = routers;
+  const axiosLogger = (req: Request, res: Response, next: NextFunction) => {
+    next();
   };
 
-  applyMiddlewares(app);
-});
-
-app.use("/accounts", accountsRouter);
-app.use("/auth", authRouter);
-app.use("/website", websiteRouter);
-app.use("/analytics", analyticsRouter);
-app.use("/data", dataRouter);
-app.use("/gql", gqlRouter);
-
-app.get("/areyoualive", (_, res) => {
-  res.json({ answer: "yes", version: process.env.npm_package_version });
-});
-
-if (process.env.NODE_ENV === "production") {
-  app.use(
-    "/admin/queues",
-    expressBasicAuth({
-      users: { [`${process.env.ADMIN_USER}`]: `${process.env.ADMIN_PASSWORD}` },
-      challenge: true,
-      realm: "Imb4T3st4pp",
+  ocServerDomain = `http://${ip}:6777`;
+  const middlewares = [
+    cookieParser(),
+    express.json({ limit: "50mb" }),
+    express.urlencoded({ limit: "50mb", extended: true }),
+    cors({
+      origin: [ocClientDomain, clientDomain],
+      credentials: true,
     }),
-    serverAdapter.getRouter()
-  );
-} else {
-  app.use("/admin/queues", serverAdapter.getRouter());
-}
+    axiosLogger,
+  ];
+
+  middlewares.forEach((middleware) => app.use(middleware));
+
+  app.use("/accounts", accountsRouter);
+  app.use("/auth", authRouter);
+  app.use("/website", websiteRouter);
+  app.use("/analytics", analyticsRouter);
+  app.use("/data", dataRouter);
+  app.use("/gql", gqlRouter);
+
+  app.get("/areyoualive", (_, res) => {
+    res.json({ answer: "yes", version: process.env.npm_package_version });
+  });
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(
+      "/admin/queues",
+      expressBasicAuth({
+        users: {
+          [`${process.env.ADMIN_USER}`]: `${process.env.ADMIN_PASSWORD}`,
+        },
+        challenge: true,
+        realm: "Imb4T3st4pp",
+      }),
+      serverAdapter.getRouter()
+    );
+  } else {
+    app.use("/admin/queues", serverAdapter.getRouter());
+  }
+});
