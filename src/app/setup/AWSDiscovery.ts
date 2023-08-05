7fcd0b6c0ad24912ba04dfcdd0f23571d57d5442
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-05 16:22:08 +0300

    af

--- src/app/setup/AWSDiscovery.ts
+++ src/app/setup/AWSDiscovery.ts
@@ -1,9 +1,12 @@
 import { ServiceDiscovery } from "@aws-sdk/client-servicediscovery";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const discoverService = async (
   region: string,
   params: any
 ): Promise<string> => {
+  if (process.env.NODE_ENV === "development") return "127.0.0.1";
   const serviceDiscovery = new ServiceDiscovery({ region });
   try {
     const data = await serviceDiscovery.discoverInstances(params);
