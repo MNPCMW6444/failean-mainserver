b65b57a98aadf33ecd9107bfcc6cd3a8bf73de2b
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-05 14:27:32 +0300

    awd

--- src/app/setup/AWSDiscovery.ts
+++ src/app/setup/AWSDiscovery.ts
@@ -1,19 +1,10 @@
 import { ServiceDiscovery } from "@aws-sdk/client-servicediscovery";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const discoverService = async (
   region: string,
   params: any
 ): Promise<string> => {
-  if (process.env.NODE_ENV === "development") return "127.0.0.1";
-  const serviceDiscovery = new ServiceDiscovery({
-    region,
-    credentials: {
-      accessKeyId: "AKIA6MGDYZ6MAU2NZXTS",
-      secretAccessKey: "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",
-    },
-  });
+  const serviceDiscovery = new ServiceDiscovery({ region });
   try {
     const data = await serviceDiscovery.discoverInstances(params);
     return (
