24db1cfc22c72fad695c8faac5e0bc09e3fc27a9
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-09-08 23:47:20 +0300

    g

--- src/app/setup/config.ts
+++ src/app/setup/config.ts
@@ -9,4 +9,4 @@ export const ocClientDomain =
 export const clientDomain =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5999"
-    : "https://failean.com";
+    : "https://app.failean.com";
