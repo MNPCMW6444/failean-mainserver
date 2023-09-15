a405fe5759ed6b1407d1b76e69e3f324b879eb03
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-09-15 17:40:57 +0300

    vcxz

--- src/app/jobs/openAIQueue.ts
+++ src/app/jobs/openAIQueue.ts
@@ -1,209 +1,204 @@
 import pubsub, {openAIQueue} from "../setup/redisSetup";
-import { getIdeaModel } from "../mongo-models/data/ideas/ideaModel";
+import {getIdeaModel} from "../mongo-models/data/ideas/ideaModel";
 import aideatorPromptMap from "../../content/prompts/aideatorPromptMap";
 import {
-  API,
-  PromptName,
-  PromptPart,
-  WhiteModels,
+    API,
+    PromptName,
+    PromptPart,
+    WhiteModels,
 } from "@failean/shared-types";
-import { getPromptResultModel } from "../mongo-models/data/prompts/promptResultModel";
-import { callOpenAI } from "../util/data/prompts/openAIUtil";
-import { createBullBoard } from "@bull-board/api";
-import { BullAdapter } from "@bull-board/api/bullAdapter";
-import { ExpressAdapter } from "@bull-board/express";
+import {getPromptResultModel} from "../mongo-models/data/prompts/promptResultModel";
+import {callOpenAI} from "../util/data/prompts/openAIUtil";
+import {createBullBoard} from "@bull-board/api";
+import {BullAdapter} from "@bull-board/api/bullAdapter";
+import {ExpressAdapter} from "@bull-board/express";
 import stringSimilarity from "../util/string-similarity";
-import { INVALID_PROMPT_MESSAGE } from "../util/messages";
-import { safeStringify } from "../util/jsonUtil";
-import {axiosInstance } from "@failean/oc-server-axiosinstance"
+import {INVALID_PROMPT_MESSAGE} from "../util/messages";
+import {safeStringify} from "../util/jsonUtil";
+import {axiosInstance} from "@failean/oc-server-axiosinstance"
 
 
 export const serverAdapter = new ExpressAdapter();
 
 const processJob = async (job: any) => {
-  const PromptResultModel = getPromptResultModel();
-  const ideaModel = getIdeaModel();
-  try {
-    const { user, ideaID, promptName, feedback, reqUUID } = job.data;
-    if (user.subscription !== "tokens") {
-      return;
-    }
-    const idea = await ideaModel.findById(ideaID);
-    let dependencies: string[];
-    const prompt = aideatorPromptMap[promptName];
-    if (prompt) {
-      let promises = prompt.prompt.map(async (promptPart: PromptPart) => {
-        if (promptPart.type === "variable" && promptPart.content !== "idea") {
-          let promptRes = await PromptResultModel.find({
-            owner: user._id,
-            ideaID,
-            promptName: promptPart.content,
-          });
-          return {
-            x: promptRes[promptRes.length - 1]?.data,
-          };
+    const PromptResultModel = getPromptResultModel();
+    const ideaModel = getIdeaModel();
+    try {
+        const {user, ideaID, promptName, feedback, reqUUID} = job.data;
+        if (user.subscription !== "tokens") {
+            return;
         }
-      });
+        const idea = await ideaModel.findById(ideaID);
+        let dependencies: string[];
+        const prompt = aideatorPromptMap[promptName];
+        if (prompt) {
+            let promises = prompt.prompt.map(async (promptPart: PromptPart) => {
+                if (promptPart.type === "variable" && promptPart.content !== "idea") {
+                    let promptRes = await PromptResultModel.find({
+                        owner: user._id,
+                        ideaID,
+                        promptName: promptPart.content,
+                    });
+                    return {
+                        x: promptRes[promptRes.length - 1]?.data,
+                    };
+                }
+            });
 
-      await Promise.all(promises).then(async (updatedPropmtResult) => {
-        dependencies = updatedPropmtResult.map((r: any) => {
-          return r;
-        });
+            await Promise.all(promises).then(async (updatedPropmtResult) => {
+                dependencies = updatedPropmtResult.map((r: any) => {
+                    return r;
+                });
 
-        const cleanDeps: string[] = [];
-        dependencies.forEach((dep) => {
-          if (dep) cleanDeps.push(dep);
-        });
-        let i = 0;
+                const cleanDeps: string[] = [];
+                dependencies.forEach((dep) => {
+                    if (dep && dep !== INVALID_PROMPT_MESSAGE) cleanDeps.push(dep);
+                });
+                let i = 0;
 
-        let missing = false;
+                let missing = false;
 
-        const constructedPrompt = prompt.prompt.map(
-            (promptPart: PromptPart) => {
-              if (promptPart.type === "static") return promptPart.content;
-              else if (promptPart.type === "variable") {
-                if (promptPart.content === "idea") return idea?.idea;
-                i++;
-                const res = (cleanDeps[i - 1] as any)?.x;
-                missing = !missing && !(res?.length > 1);
-                return res;
-              }
-            }
-        );
+                const constructedPrompt = prompt.prompt.map(
+                    (promptPart: PromptPart) => {
+                        if (promptPart.type === "static") return promptPart.content;
+                        else if (promptPart.type === "variable") {
+                            if (promptPart.content === "idea") return idea?.idea;
+                            i++;
+                            const res = (cleanDeps[i - 1] as any)?.x;
+                            missing = !missing && !(res?.length > 1);
+                            return res;
+                        }
+                    }
+                );
 
-        if (missing) throw new Error("Missing dependencies");
+                if (missing) throw new Error("Missing dependencies");
 
-        const promptResult =
-            feedback?.length &&
-            feedback?.length > 1 &&
-            (await PromptResultModel.find({
-              owner: user._id,
-              ideaID,
-              promptName,
-            }));
-
-
-        const completion = await callOpenAI(
-            user as unknown as WhiteUser,
-            prompt.role,
-            promptResult && [promptResult.length - 1] &&
-            promptResult[promptResult.length - 1].data
-                ? [
-                  { role: "user", content: constructedPrompt.join("") },
-                  {
-                    role: "assistant",
-                    content: promptResult[promptResult.length - 1].data,
-                  },
-                  { role: "user", content: feedback },
-                ]
-                : [{ role: "user", content: constructedPrompt.join("") }],
-            promptName,
-            reqUUID
-        );
+                const promptResult =
+                    feedback?.length &&
+                    feedback?.length > 1 &&
+                    (await PromptResultModel.find({
+                        owner: user._id,
+                        ideaID,
+                        promptName,
+                    }));
 
 
-        if (completion === -1) throw new Error("Acoount error");
-        else if (completion === -2) throw new Error("No Tokens");
-        else {
-          if (
-              stringSimilarity(
-                  (completion as any).choices[0].message?.content + "",
-                  INVALID_PROMPT_MESSAGE
-              ) > 0.6
-          )
-            axiosInstance
-                .post("log/logInvalidPrompt", {
-                  stringifiedCompletion: safeStringify(completion),
-                  prompt: constructedPrompt.join(""),
-                  result: (completion as any).choices[0].message?.content,
-                  promptName: promptName.length?.length ? promptName.join(" "):promptName,
-                  ideaID,
-                })
-                .catch((err: any) => console.error(err));
-          const savedResult = new PromptResultModel({
-            owner: user._id,
-            ideaID,
-            promptName,
-            data: (completion as any).choices[0].message?.content,
-            reason:
-                feedback?.length && feedback?.length > 1 ? "feedback" : "run",
-          });
-          await savedResult.save();
+                const completion = await callOpenAI(
+                    user as unknown as WhiteUser,
+                    prompt.role,
+                    promptResult && [promptResult.length - 1] &&
+                    promptResult[promptResult.length - 1].data
+                        ? [
+                            {role: "user", content: constructedPrompt.join("")},
+                            {
+                                role: "assistant",
+                                content: promptResult[promptResult.length - 1].data,
+                            },
+                            {role: "user", content: feedback},
+                        ]
+                        : [{role: "user", content: constructedPrompt.join("")}],
+                    promptName,
+                    reqUUID
+                );
+
+
+                if (completion === -1) throw new Error("Acoount error");
+                else if (completion === -2) throw new Error("No Tokens");
+                else {
+                    if (
+                        stringSimilarity(
+                            (completion as any).choices[0].message?.content + "",
+                            INVALID_PROMPT_MESSAGE
+                        ) > 0.6
+                    )
+                        axiosInstance
+                            .post("log/logInvalidPrompt", {
+                                stringifiedCompletion: safeStringify(completion),
+                                prompt: constructedPrompt.join(""),
+                                result: (completion as any).choices[0].message?.content,
+                                promptName: promptName.length?.length ? promptName.join(" ") : promptName,
+                                ideaID,
+                            })
+                            .catch((err: any) => console.error(err));
+                    const savedResult = new PromptResultModel({
+                        owner: user._id,
+                        ideaID,
+                        promptName,
+                        data: (completion as any).choices[0].message?.content,
+                        reason:
+                            feedback?.length && feedback?.length > 1 ? "feedback" : "run",
+                    });
+                    await savedResult.save();
+                }
+            });
         }
-      });
+        pubsub.publish("JOB_COMPLETED", {
+            jobCompleted: (job?.id || "8765") + "",
+        });
+        console.log("Published update for job ", {
+            jobCompleted: (job?.id || "8765") + "",
+        });
+    } catch (error) {
+        console.error(`An error occurred during job processing: ${error}`);
+        pubsub.publish("JOB_COMPLETED", {
+            jobCompleted: (job?.id || "8765") + "",
+            status: "error",
+            message: error,
+        });
     }
-    pubsub.publish("JOB_COMPLETED", {
-      jobCompleted: (job?.id || "8765") + "",
-    });
-    console.log("Published update for job ", {
-      jobCompleted: (job?.id || "8765") + "",
-    });
-  } catch (error) {
-    console.error(`An error occurred during job processing: ${error}`);
-    pubsub.publish("JOB_COMPLETED", {
-      jobCompleted: (job?.id || "8765") + "",
-      status: "error",
-      message: error,
-    });
-  }
 };
-
 
 
 type WhiteUser = WhiteModels.Auth.WhiteUser;
 
 
+serverAdapter.setBasePath("/admin/queues");
 
+openAIQueue.on("error", (error: any) => {
+    console.error(`A queue error happened: ${error}`);
+});
 
+createBullBoard({
+    queues: [new BullAdapter(openAIQueue)],
+    serverAdapter: serverAdapter,
+});
 
-
-    serverAdapter.setBasePath("/admin/queues");
-
-    openAIQueue.on("error", (error: any) => {
-     console.error(`A queue error happened: ${error}`);
-    });
-
-    createBullBoard({
-      queues: [new BullAdapter(openAIQueue)],
-      serverAdapter: serverAdapter,
-    });
-
-    // Process jobs using the processJob function
-    openAIQueue.process(processJob);
+// Process jobs using the processJob function
+openAIQueue.process(processJob);
 
 
 // Define your job processing function
 
 export const addJobsToQueue = async (
-  user: WhiteModels.Auth.WhiteUser,
-  ideaID: string,
-  promptNames: PromptName[],
-  feedback: API.Data.RunAndGetPromptResult.Req["feedback"],
-  req: any
-) => {
-  const addJobToQueue = async (
     user: WhiteModels.Auth.WhiteUser,
     ideaID: string,
-    promptName: PromptName,
+    promptNames: PromptName[],
     feedback: API.Data.RunAndGetPromptResult.Req["feedback"],
     req: any
-  ) => {
-    await openAIQueue
-      .add({ user, ideaID, promptName, feedback, reqUUID: req.uuid })
-      .then((job: any) => {
-        job.finished().then(() => {
-          promptNames.shift();
-          if (promptNames[0] === "idea") promptNames.shift();
-          if (promptNames.length > 0) {
-            addJobToQueue(user, ideaID, promptNames[0], feedback, req);
-          }
-        });
-      });
-  };
+) => {
+    const addJobToQueue = async (
+        user: WhiteModels.Auth.WhiteUser,
+        ideaID: string,
+        promptName: PromptName,
+        feedback: API.Data.RunAndGetPromptResult.Req["feedback"],
+        req: any
+    ) => {
+        await openAIQueue
+            .add({user, ideaID, promptName, feedback, reqUUID: req.uuid})
+            .then((job: any) => {
+                job.finished().then(() => {
+                    promptNames.shift();
+                    if (promptNames[0] === "idea") promptNames.shift();
+                    if (promptNames.length > 0) {
+                        addJobToQueue(user, ideaID, promptNames[0], feedback, req);
+                    }
+                });
+            });
+    };
 
-  if (promptNames.length > 0) {
-    await addJobToQueue(user, ideaID, promptNames[0], feedback, req);
-  }
+    if (promptNames.length > 0) {
+        await addJobToQueue(user, ideaID, promptNames[0], feedback, req);
+    }
 };
 
 export default openAIQueue;
--- src/app/routers/data/prompts/promptsRouter.ts
+++ src/app/routers/data/prompts/promptsRouter.ts
@@ -1,191 +1,227 @@
 import express from "express";
-import { getPromptResultModel } from "../../../mongo-models/data/prompts/promptResultModel";
-import { PromptName } from "@failean/shared-types";
-import { addJobsToQueue } from "../../../jobs/openAIQueue";
+import {getPromptResultModel} from "../../../mongo-models/data/prompts/promptResultModel";
+import {PromptName} from "@failean/shared-types";
+import openAIQueue, {addJobsToQueue} from "../../../jobs/openAIQueue";
 import {
-  convertMaptoDeckGraph,
-  convertMaptoDepGraph,
+    convertMaptoDeckGraph,
+    convertMaptoDepGraph,
 } from "../../../util/data/prompts/promptUtil";
 import aideatorPromptMap from "../../../../content/prompts/aideatorPromptMap";
-import { authUser } from "../../../util/authUtil";
-import { API } from "@failean/shared-types";
-import { estimateOpenAI } from "../../../util/data/prompts/openAIUtil";
-import { tokenCount } from "../../../util/accounts/tokensUtil";
+import {authUser} from "../../../util/authUtil";
+import {API} from "@failean/shared-types";
+import {estimateOpenAI} from "../../../util/data/prompts/openAIUtil";
+import {tokenCount} from "../../../util/accounts/tokensUtil";
 import {axiosInstance} from "@failean/oc-server-axiosinstance";
+import {JobStatus} from "bull";
 
 const router = express.Router();
 
 router.get("/getPromptGraph", async (_, res) => {
-  try {
-    const graph = convertMaptoDepGraph(aideatorPromptMap);
-    return res.status(200).json({
-      graph,
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ errorMessage: JSON.stringify(err) });
-  }
+    try {
+        const graph = convertMaptoDepGraph(aideatorPromptMap);
+        return res.status(200).json({
+            graph,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({errorMessage: JSON.stringify(err)});
+    }
 });
 
 router.get("/getDeckPromptGraph", async (_, res) => {
-  try {
-    const graph = convertMaptoDeckGraph();
-    return res.status(200).json({
-      graph,
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ errorMessage: JSON.stringify(err) });
-  }
+    try {
+        const graph = convertMaptoDeckGraph();
+        return res.status(200).json({
+            graph,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({errorMessage: JSON.stringify(err)});
+    }
 });
 
 router.post("/getPromptResult", async (req, res) => {
-  try {
-    const user = await authUser(req.cookies.jsonwebtoken);
+    try {
+        const user = await authUser(req.cookies.jsonwebtoken);
 
-    if (!user) {
-      return res.status(401).json({ clientMessage: "Unauthorized" });
+        if (!user) {
+            return res.status(401).json({clientMessage: "Unauthorized"});
+        }
+
+        const {ideaID, promptName}: { ideaID: string; promptName: PromptName } =
+            req.body;
+
+        const PromptResultModel = getPromptResultModel();
+
+        if (promptName === "all") {
+            const promptResults = await PromptResultModel.find({
+                owner: user._id,
+                ideaID,
+            });
+
+            return res.status(200).json({
+                promptResult: promptResults,
+            });
+        }
+
+        const promptResult = await PromptResultModel.find({
+            owner: user._id,
+            ideaID,
+            promptName,
+        });
+
+        return res.status(200).json({
+            promptResult: promptResult[promptResult.length - 1],
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({errorMessage: JSON.stringify(err)});
     }
-
-    const { ideaID, promptName }: { ideaID: string; promptName: PromptName } =
-      req.body;
-
-    const PromptResultModel = getPromptResultModel();
-
-    if (promptName === "all") {
-      const promptResults = await PromptResultModel.find({
-        owner: user._id,
-        ideaID,
-      });
-
-      return res.status(200).json({
-        promptResult: promptResults,
-      });
-    }
-
-    const promptResult = await PromptResultModel.find({
-      owner: user._id,
-      ideaID,
-      promptName,
-    });
-
-    return res.status(200).json({
-      promptResult: promptResult[promptResult.length - 1],
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ errorMessage: JSON.stringify(err) });
-  }
 });
 
 router.post("/preRunPrompt", async (req, res) => {
-  try {
-    const user = await authUser(req.cookies.jsonwebtoken);
-
-    if (!user) {
-      return res.status(401).json({ clientMessage: "Unauthorized" });
-    }
-
-    const {
-      ideaID,
-      promptNames,
-      feedback,
-    }: API.Data.RunAndGetPromptResult.Req = req.body;
-
-    let avgx: number | undefined ;
-
     try {
-      const avg =( await axiosInstance.post("log/logPromptPrice", {
-        promptName: promptNames,
-      })).data.avg;
-      if (avg !== "no") avgx = avg;
+        const user = await authUser(req.cookies.jsonwebtoken);
 
-    } catch (e) {
-      console.log("estimated kaki");
-      avgx = await Promise.all(
-        promptNames.map((promptName) =>
-          estimateOpenAI(user, ideaID, promptName, feedback)
-        )
-      ).then((results) =>
-        results.reduce(
-          (total, number) =>
-            (total !== null && total !== undefined ? total : 99999999) +
-            (number !== null && number !== undefined ? number : 99999999),
-          0
-        )
-      );
+        if (!user) {
+            return res.status(401).json({clientMessage: "Unauthorized"});
+        }
+
+        const {
+            ideaID,
+            promptNames,
+            feedback,
+        }: API.Data.RunAndGetPromptResult.Req = req.body;
+
+        let avgx: number | undefined;
+
+        try {
+            const avg = (await axiosInstance.post("log/logPromptPrice", {
+                promptName: promptNames,
+            })).data.avg;
+            if (avg !== "no") avgx = avg;
+
+        } catch (e) {
+            console.log("estimated kaki");
+            avgx = await Promise.all(
+                promptNames.map((promptName) =>
+                    estimateOpenAI(user, ideaID, promptName, feedback)
+                )
+            ).then((results) =>
+                results.reduce(
+                    (total, number) =>
+                        (total !== null && total !== undefined ? total : 99999999) +
+                        (number !== null && number !== undefined ? number : 99999999),
+                    0
+                )
+            );
+        }
+        return res.status(200).json({price: avgx});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({clientMessage: "An error occurred"});
     }
-    return res.status(200).json({ price: avgx });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ clientMessage: "An error occurred" });
-  }
 });
 
 router.post("/runAndGetPromptResult", async (req, res) => {
-  try {
-    const user = await authUser(req.cookies.jsonwebtoken);
+    try {
+        const user = await authUser(req.cookies.jsonwebtoken);
 
-    if (!user) {
-      return res.status(401).json({ clientMessage: "Unauthorized" });
+        if (!user) {
+            return res.status(401).json({clientMessage: "Unauthorized"});
+        }
+
+        if ((await tokenCount(user._id)) <= 0)
+            return res
+                .status(400)
+                .json({clientMessage: "Token balance is not positive..."});
+
+        const {
+            ideaID,
+            promptNames,
+            feedback,
+        }: API.Data.RunAndGetPromptResult.Req = req.body;
+
+        await addJobsToQueue(user, ideaID, promptNames, feedback, req);
+
+        return res.status(200).json({addedJobSequence: true});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({clientMessage: "An error occurred"});
     }
-
-    if ((await tokenCount(user._id)) <= 0)
-      return res
-        .status(400)
-        .json({ clientMessage: "Token balance is not positive..." });
-
-    const {
-      ideaID,
-      promptNames,
-      feedback,
-    }: API.Data.RunAndGetPromptResult.Req = req.body;
-
-    await addJobsToQueue(user, ideaID, promptNames, feedback, req);
-
-    return res.status(200).json({ addedJobSequence: true });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ clientMessage: "An error occurred" });
-  }
 });
 
 router.post("/savePromptResult", async (req, res) => {
-  try {
-    const user = await authUser(req.cookies.jsonwebtoken);
+    try {
+        const user = await authUser(req.cookies.jsonwebtoken);
 
-    if (!user) {
-      return res.status(401).json({ clientMessage: "Unauthorized" });
+        if (!user) {
+            return res.status(401).json({clientMessage: "Unauthorized"});
+        }
+
+        const {
+            ideaID,
+            promptName,
+            data,
+            reason,
+        }: {
+            ideaID: string;
+            promptName: PromptName;
+            data: string;
+            reason: string;
+        } = req.body;
+        const PromptResultModel = getPromptResultModel();
+
+        const savedPromptResult = new PromptResultModel({
+            owner: user._id,
+            ideaID,
+            promptName,
+            data,
+            reason,
+        });
+        await savedPromptResult.save();
+        return res.status(200).json({
+            response: savedPromptResult.data,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({errorMessage: JSON.stringify(err)});
     }
+});
 
-    const {
-      ideaID,
-      promptName,
-      data,
-      reason,
-    }: {
-      ideaID: string;
-      promptName: PromptName;
-      data: string;
-      reason: string;
-    } = req.body;
-    const PromptResultModel = getPromptResultModel();
+router.get("/tasks", async (req, res) => {
+    try {
+        const user = await authUser(req.cookies.jsonwebtoken);
+        if (!user) {
+            return res.status(401).json({message: 'Unauthorized'});
+        }
 
-    const savedPromptResult = new PromptResultModel({
-      owner: user._id,
-      ideaID,
-      promptName,
-      data,
-      reason,
-    });
-    await savedPromptResult.save();
-    return res.status(200).json({
-      response: savedPromptResult.data,
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ errorMessage: JSON.stringify(err) });
-  }
+        const statuses: JobStatus[] = ['completed', 'waiting', 'active', 'delayed', 'failed', 'paused'];
+        const allJobs = [];
+
+        for (const status of statuses) {
+            const jobs = await openAIQueue.getJobs([status]);
+            const filteredJobs = jobs.filter(job => job.data.user._id === user._id.toString());
+
+            // Transform the data
+            const transformedJobs = filteredJobs.map((job: any) => {
+                return {
+                    data: job.data.promptName,
+                    id: job.id,
+                    status,
+                    timestamp: job.timestamp
+                };
+            });
+
+            allJobs.push(...transformedJobs);
+        }
+
+        return res.status(200).json({data: allJobs});
+
+    } catch (error) {
+        console.error('Error:', error);
+        return res.status(500).json({message: 'An error occurred'});
+    }
 });
 
 export default router;
