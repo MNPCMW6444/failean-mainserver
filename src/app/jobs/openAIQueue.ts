86e6f4053bc4794004ac8af4b5434ad0e6a0c4a4
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-09-16 01:09:20 +0300

    Update openAIQueue.ts

--- src/app/jobs/openAIQueue.ts
+++ src/app/jobs/openAIQueue.ts
@@ -89,7 +89,7 @@ openAIQueue.process(async (job) => {
 
 
                 const chat = promptResult && [promptResult.length - 1] &&
-                promptResult[promptResult.length - 1].data
+                promptResult[promptResult.length - 1]?.data
                     ? [
                         {role: "user", content: constructedPrompt.join("")},
                         {
@@ -180,7 +180,7 @@ openAIQueue.process(async (job) => {
             = await getAITaskModel().findById(job.data.taskID);
         if (task) {
             task.status = "failed"
-            task.promptResIDOrReason = error === "Missing dependencies" ? "missing" : error === "No Tokens" ? "Out Of Tokens" : error.toString().substring(7, error.toString().length)
+            task.promptResIDOrReason = error === "Missing dependencies" ? "missing" : error === "No Tokens" ? "Out Of Tokens" : error.toString().substring(11, error.toString().length)
             task.finishTime = new Date();
         }
         await task?.save()
