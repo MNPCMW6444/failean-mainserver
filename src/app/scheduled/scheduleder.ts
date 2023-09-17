07134bbf70d75874a86e484de6b7c577f3fc8444
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-09-17 15:42:33 +0300

    Update scheduleder.ts

--- src/app/scheduled/scheduleder.ts
+++ src/app/scheduled/scheduleder.ts
@@ -1,70 +1,71 @@
-import { getEmailModel } from "../mongo-models/abtest/emailModel";
-import { sendEmail } from "../util/emailUtil";
-import { waitListReady } from "../../content/email-templates/authEmails";
-import { getAITaskModel } from "../mongo-models/tasks/openAITaskModel";
+import {getEmailModel} from "../mongo-models/abtest/emailModel";
+import {sendEmail} from "../util/emailUtil";
+import {waitListReady} from "../../content/email-templates/authEmails";
+import {getAITaskModel} from "../mongo-models/tasks/openAITaskModel";
 import axios from "axios";
 
 const ONE_WEEK_IN_MS = 1000 * 60 * 60 * 24 * 7;
 const TEN_MINUTES_IN_MS = 600000;
 
 const scheduleWaitingListReadyEmail = () => {
-  setInterval(async () => {
-    try {
-      const emailDocs = await getEmailModel().find();
-      for (const doc of emailDocs) {
-        if (
-          !doc.emailSent &&
-          doc.product === "failean" &&
-          doc.email &&
-          new Date().getTime() - doc.createdAt.getTime() > ONE_WEEK_IN_MS
-        ) {
-          const { subject, body } = waitListReady("Unknown");
-          const [res] = await sendEmail(doc.email, subject, body);
-          if (res.statusCode < 299 && res.statusCode >= 200) {
-            doc.emailSent = "yes";
-            await doc.save();
-          }
+    setInterval(async () => {
+        try {
+            const emailDocs = await getEmailModel().find();
+            for (const doc of emailDocs) {
+                if (
+                    !doc.emailSent &&
+                    doc.product === "failean" &&
+                    doc.email &&
+                    new Date().getTime() - doc.createdAt.getTime() > ONE_WEEK_IN_MS
+                ) {
+                    const {subject, body} = waitListReady("Unknown");
+                    const [res] = await sendEmail(doc.email, subject, body);
+                    if (res.statusCode < 299 && res.statusCode >= 200) {
+                        doc.emailSent = "yes";
+                        await doc.save();
+                    }
+                }
+            }
+        } catch (error) {
+            console.error("An error occurred:", error);
         }
-      }
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  }, TEN_MINUTES_IN_MS * 2);
+    }, TEN_MINUTES_IN_MS * 2);
 };
 
 const cleanOldTasks = () => {
-  setInterval(async () => {
-    try {
-      (await getAITaskModel().find()).forEach((task) => {
-        if (
-          new Date().getTime() - task.startTime.getTime() >
-          TEN_MINUTES_IN_MS / 2
-        ) {
-          task.status = "failed";
-          task.promptResIDOrReason = "timeout";
-          task.finishTime = new Date();
-          task.save();
+    setInterval(async () => {
+        try {
+            (await getAITaskModel().find()).forEach((task) => {
+                if (
+                    new Date().getTime() - task.startTime.getTime() >
+                    TEN_MINUTES_IN_MS / 2
+                ) {
+                    task.status = "failed";
+                    task.promptResIDOrReason = "timeout";
+                    task.finishTime = new Date();
+                    if (!task.promptName) task.promptName = "unknown";
+                    task.save();
+                }
+            });
+        } catch (error) {
+            console.error("An error occurred:", error);
         }
-      });
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  }, TEN_MINUTES_IN_MS / 3);
+    }, TEN_MINUTES_IN_MS / 3);
 };
 
 const redirectLive = () => {
-  setInterval(async () => {
-    try {
-      axios.get("https://app.failean.com/");
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  }, TEN_MINUTES_IN_MS / 2);
+    setInterval(async () => {
+        try {
+            axios.get("https://app.failean.com/");
+        } catch (error) {
+            console.error("An error occurred:", error);
+        }
+    }, TEN_MINUTES_IN_MS / 2);
 };
 
 const scheduleAll = () => {
-  scheduleWaitingListReadyEmail();
-  cleanOldTasks();
-  redirectLive();
+    scheduleWaitingListReadyEmail();
+    cleanOldTasks();
+    redirectLive();
 };
 export default scheduleAll;
